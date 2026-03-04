3535ce1d0c1e2f13455c20c876d45912b9b3d060
ndtobs <ndtobs@users.noreply.github.com>
2026-03-04 19:29:54 +0000

    fix: add $lib path alias to vite resolve config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://v2.tauri.app/start/frontend/svelte/
 export default defineConfig({
   plugins: [svelte()],
+
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
 
   // Prevent vite from obscuring Rust errors
   clearScreen: false,
